1724adcaf0537f7bd2934341a0644f08945a3f9a
Anders Jepsen <anders@magenta.dk>
2019-01-09 09:59:11 +0100

    minor improvements

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import ApplicationForm from './views/ApplicationForm'
+import ThankYou from './views/ThankYou'
 
 Vue.use(Router)
 
@@ -15,10 +16,7 @@ export default new Router({
     {
       path: '/tak',
       name: 'thankyou',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/ThankYou.vue')
+      component: ThankYou
     }
   ]
 })
