9cf57b6354718e8c5d8493640ba16c0a6494ac3f
Toke <toke@MacBook-Pro.local>
2019-02-18 13:24:34 +0100

    Changed content-type header for Axios POST request

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,13 @@ const BASE_URL = process.env.VUE_APP_API_ENDPOINT
 export default {
   sendApplication (formData) {
     const url = `${BASE_URL}`
-    return axios.post(url, formData)
+    return axios.post({
+      url: url,
+      body: formData,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
       .then(response => {
         console.log(response)
         return response.data
